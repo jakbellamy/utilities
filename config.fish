e7bdf501f9d6f7b676654f09bdff3aadb704ed21
github username <github email address>
2020-03-15 20:39:03 -0400

    added fish aliases

---
+++ config.fish
@@ -0,0 +1,8 @@
+#aliases
+  d0='npm i express cors body-parser'
+  d1='touch index.js && mkdir src && cd src/ && mkdir models && mkdir controllers && mkdir routes && cd routes && touch routes.js && cd .. && cd .. && atom . && npm i express cors lodash body-parser mongoose'
+  dev='cd && cd Dev/'
+  supreme='cd && cd Dev/_supreme/supremeMongo'
+  supremeStart='supreme && atom . && npm start'
+  sandbox="cd && cd Dev/_sandbox/ && mkdir (date +'%Y-%m-%d') && cd (date +'%Y-%m-%d') && touch script.js && atom ." --
+  save
